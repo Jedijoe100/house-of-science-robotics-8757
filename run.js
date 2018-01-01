8933a64be632c45843d86225f8ecb1d013f9fa5d
DESKTOP-3LEPBII\Joe Kent <thisisjedijoe@gmail.com>
2018-01-02 06:56:43 +1300

    should fix the error

--- run.js
+++ run.js
@@ -3,7 +3,7 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 var path = require('path');
-var path1 = '';
+var path1 = '/';
 http.createServer(function (req, res) {
 	var q = url.parse(req.url, true);
 	var filename = q.pathname;
@@ -14,7 +14,8 @@ http.createServer(function (req, res) {
 		if (err) {
 			res.writeHead(404, {'Content-Type': 'text/html'});
 			return res.end("404 Not Found");
-		}  
+		}
+		
 		switch (filetype){
 			case '.html':
 			res.writeHead(200, {'Content-Type': 'text/html'});
