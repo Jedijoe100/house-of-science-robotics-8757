02559ea9cf725a1fe13e2719f937d1bb7455e5c8
DESKTOP-3LEPBII\Joe Kent <thisisjedijoe@gmail.com>
2018-01-03 08:30:40 +1300

    fixed an error

--- run.js
+++ run.js
@@ -3,13 +3,13 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 var path = require('path');
-var path1 = '\';
+var path1 = "/";
 http.createServer(function (req, res) {
 	var q = url.parse(req.url, true);
 	var filename = q.pathname;
 	var fileloc = path1 + filename;
 	var filetype = path.extname(filename);
-	fs.readFile(fileloc, (err, data) => {
+	fs.readFile(fileloc, function(err, data) {
 		var content = data;
 		if (err) {
 			res.writeHead(404, {'Content-Type': 'text/html'});
