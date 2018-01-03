45935dd05c54aeae22d77170fe7982f80d9759cb
DESKTOP-3LEPBII\Joe Kent <thisisjedijoe@gmail.com>
2018-01-04 10:24:16 +1300

    test

--- run.js
+++ run.js
@@ -10,7 +10,7 @@ express()
     .get('/people', (req, res) => res.render('pages/people'))
     .get('/club', (req, res) => res.render('pages/club'))
     .get('/robots', (req, res) => res.render('pages/robots'))
-    .get('/articles', (req, res) => res.contentType('text/xml').render('pages/artiles'))
+    .get('/articles', (req, res) => res.contentType('text/xml').render('pages/articles'))
     //.get('/articles', (req, res) => res.render('pages/articles'))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
